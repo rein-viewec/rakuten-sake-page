8a0b24b1be8c795fc5c717972e3c2454adda578d
rein-viewec <83944723+rein-viewec@users.noreply.github.com>
2021-12-02 11:41:35 +0800

    feat: add hover images

--- js/main.js
+++ js/main.js
@@ -1,44 +1,5 @@
-
-const tabSetting = {
-  slidesPerView: 'auto',
-  spaceBetween: 0,
-  grabCursor: true,
-  autoplay: {
-    delay: 6000,
-    disableOnInteraction: false,
-  },
-}
-const tabItemSetting = {
-  slidesPerView: 2,
-  spaceBetween: 0,
-  grabCursor: true,
-  breakpoints: {
-    '720': {
-      slidesPerView: 'auto',
-      spaceBetween: 0,
-    },
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-}
 $(document).ready(() => {
-  $('#Sidebar').load('sidebar.html')
-  $('#Footer').load('footer.html')
-  $('.item__tab__new').tab()
-  $('.item__tab__hot').tab()
   AOS.init({
     once: true,
   })
-  new Swiper('.item__tab__new', tabSetting)
-  new Swiper('.tab__content', tabItemSetting)
 })
